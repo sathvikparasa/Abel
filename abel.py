729939b315e95488a8ba4f5f6f4ec51689545db3
Sathvik Parasa <sathvikparasa007@gmail.com>
2021-12-28 22:41:09 -0800

    Add main, train and load weights in abel

--- abel.py
+++ abel.py
@@ -311,3 +311,209 @@ def transformer(vocab_size,
   outputs = tf.keras.layers.Dense(units=vocab_size, name="outputs")(dec_outputs)
 
   return tf.keras.Model(inputs=[inputs, dec_inputs], outputs=outputs, name=name)
+
+chat_data = pd.read_csv("https://raw.githubusercontent.com/nbertagnolli/counsel-chat/master/data/20200325_counsel_chat.csv")
+
+X = chat_data['questionText']
+y = chat_data['answerText']
+
+def preprocess_text(phrase): 
+  phrase = re.sub(r"\xa0", "", phrase) # removes "\xa0"
+  phrase = re.sub(r"\n", "", phrase) # removes "\n"
+  phrase = re.sub("[.]{1,}", ".", phrase) # removes duplicate "."s
+  phrase = re.sub("[ ]{1,}", " ", phrase) # removes duplicate spaces
+  return phrase
+
+# run cleaning function
+X = X.apply(preprocess_text)
+y = y.apply(preprocess_text)
+
+question_answer_pairs = []
+
+for (question, answer) in zip(X, y):
+
+  # clean up text inputs
+
+  # example: 
+  # question = "I am not feeling well today. I feel sad."
+  # answer = "Tell me more about how you feel. What have you been up to today?"
+
+  question = preprocess_text(question) 
+  answer = preprocess_text(answer)
+
+  # split by .
+  # example
+  # question_arr = ["I am not feeling well today", "I feel sad"]
+  # answer_arr = ["Tell me more about how you feel", "What have you been up to?"]
+  question_arr = question.split(".")
+  answer_arr = answer.split(".")
+
+  # get the maximum length, which will be the shorter of the two
+  max_sentences = min(len(question_arr), len(answer_arr))
+
+  # for each combination of question + answer, pair them up
+  for i in range(max_sentences):
+
+    # set up Q/A pair
+    q_a_pair = []
+
+    # append question, answer to pair (e.g,. first sentence of question + first sentence of answer, etc.)
+    q_a_pair.append(question_arr[i])
+    q_a_pair.append(answer_arr[i])
+
+    # append to question_answer_pairs
+    question_answer_pairs.append(q_a_pair)
+
+
+# Build tokenizer using tfds for both questions and answers
+tokenizer = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+    [arr[0] + arr[1] for arr in question_answer_pairs], target_vocab_size=2**13)
+
+# Define start and end token to indicate the start and end of a sentence
+START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
+
+# Vocabulary size plus start and end token
+VOCAB_SIZE = tokenizer.vocab_size + 2
+
+MAX_LENGTH = 100 # chosen arbitrarily
+
+# tokenize, filter, pad sentences
+def tokenize_and_filter(inputs, outputs):
+  """
+    Tokenize, filter, and pad our inputs and outputs
+  """
+
+  # store results
+  tokenized_inputs, tokenized_outputs = [], []
+
+  # loop through inputs, outputs
+  for (sentence1, sentence2) in zip(inputs, outputs):
+
+    # tokenize sentence
+    sentence1 = START_TOKEN + tokenizer.encode(sentence1) + END_TOKEN
+    sentence2 = START_TOKEN + tokenizer.encode(sentence2) + END_TOKEN
+    
+    # check tokenized sentence max length
+    if len(sentence1) <= MAX_LENGTH and len(sentence2) <= MAX_LENGTH:
+      tokenized_inputs.append(sentence1)
+      tokenized_outputs.append(sentence2)
+
+  # pad tokenized sentences
+  tokenized_inputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_inputs, maxlen = MAX_LENGTH, padding = "post")
+  tokenized_outputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_outputs, maxlen = MAX_LENGTH, padding = "post")
+    
+  return tokenized_inputs, tokenized_outputs
+
+# get questions, answers
+questions, answers = tokenize_and_filter([arr[0] for arr in question_answer_pairs], 
+                                         [arr[1] for arr in question_answer_pairs])
+
+print('Vocab size: {}'.format(VOCAB_SIZE))
+print('Number of samples: {}'.format(len(questions)))
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+# decoder inputs use the previous target as input
+# remove START_TOKEN from targets
+dataset = tf.data.Dataset.from_tensor_slices((
+    {
+        'inputs': questions,
+        'dec_inputs': answers[:, :-1]
+    },
+    {
+        'outputs': answers[:, 1:]
+    },
+))
+
+dataset = dataset.cache()
+dataset = dataset.shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE)
+dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+print(dataset)
+
+tf.keras.backend.clear_session()
+
+model = transformer(vocab_size=VOCAB_SIZE, num_layers=2, units=512, d_model=256, num_heads=8, dropout=0.1)
+
+def loss_function(y_true, y_pred):
+  y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+  
+  loss = tf.keras.losses.SparseCategoricalCrossentropy(
+      from_logits=True, reduction='none')(y_true, y_pred)
+
+  mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+  loss = tf.multiply(loss, mask)
+
+  return tf.reduce_mean(loss)
+
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+  def __init__(self, d_model, warmup_steps=4000):
+    super(CustomSchedule, self).__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps**-1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+learning_rate = CustomSchedule(256)
+
+optimizer = tf.keras.optimizers.Adam(
+    learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+def accuracy(y_true, y_pred):
+  # ensure labels have shape (batch_size, MAX_LENGTH - 1)
+  y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+  return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
+
+model.compile(optimizer=optimizer, loss=loss_function, metrics=[accuracy])
+
+# model.fit(dataset, epochs=20)
+
+model.load_weights("chatbot_transformer_v4.h5")
+
+def evaluate(sentence):
+  sentence = preprocess_text(sentence)
+
+  sentence = tf.expand_dims(
+      START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+
+  output = tf.expand_dims(START_TOKEN, 0)
+
+  for i in range(MAX_LENGTH):
+    predictions = model(inputs=[sentence, output], training=False)
+
+    # select the last word from the seq_len dimension
+    predictions = predictions[:, -1:, :]
+    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+    # return the result if the predicted_id is equal to the end token
+    if tf.equal(predicted_id, END_TOKEN[0]):
+      break
+
+    # concatenated the predicted_id to the output which is given to the decoder
+    # as its input.
+    output = tf.concat([output, predicted_id], axis=-1)
+
+  return tf.squeeze(output, axis=0)
+
+def predict(sentence):
+  prediction = evaluate(sentence)
+
+  predicted_sentence = tokenizer.decode(
+      [i for i in prediction if i < tokenizer.vocab_size])
+
+  print('Abel: {}'.format(predicted_sentence))
+
+  return predicted_sentence
---
+++ main.py
@@ -0,0 +1,7 @@
+from re import L
+from abel import predict
+
+num = input("How long? ")
+num=int(num)
+for i in range(num):
+    predict(input("Sathvik: "))
